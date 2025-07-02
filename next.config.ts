0ee0fe6a6b6a4bf980c3aa80d1467edcdbf0d2cc
mynulislam32 <mynulislamsoikot1234@gmail.com>
2025-07-02 08:44:26 -0700

    sdklfj|

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true, // disables built-in image optimization
   },
+    eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
